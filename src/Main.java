e81af868547bee00b94f8b9f0db01a9a04b5988c
serj-43 <serj-43@yandex.ru>
2022-04-26 19:25:22 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int miles = 530;
+        int perMile = 20;
+        int cashBack = miles / perMile;
+        System.out.println("Вам начислено: " + cashBack + " миль");
+    }
+}
