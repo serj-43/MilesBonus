3af28268db5636bfafd9a30d57bd74ccb57825e5
serj-43 <serj-43@yandex.ru>
2022-04-30 17:12:59 +0300

    class add

---
+++ src/BonusMilesService.java
@@ -0,0 +1,6 @@
+public class BonusMilesService {
+    int calculate(int cost) {
+        int perMile = 20;
+        return cost / perMile;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int miles = 530;
-        int perMile = 20;
-        int cashBack = miles / perMile;
-        System.out.println("Вам начислено: " + cashBack + " миль");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
